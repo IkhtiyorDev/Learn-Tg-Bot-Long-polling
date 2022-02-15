6efe76bcb5b238d523780817a27b87ebf8695bde
IkhtiyorDev <ikhtiyordev@gmail.com>
2022-02-15 22:36:10 +0500

    commit

---
+++ src/main/java/dev/ikhtiyor/learntgbotlongpolling/LearnTgBotLongPollingApplication.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.learntgbotlongpolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTgBotLongPollingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnTgBotLongPollingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ikhtiyor/learntgbotlongpolling/LearnTgBotLongPollingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.learntgbotlongpolling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTgBotLongPollingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
